d4c3985dd6f714b3b3db79743544c0b414395399
Amansi-Tech <p.star.chinedu@gmail.com>
2024-08-16 16:47:28 +0100

    Amansi-codes latest PUSH

---
+++ index.js
@@ -0,0 +1,8 @@
+const body = document.querySelector('body')
+const title = document.createElement('div')
+title.innerHTML = 'emmanuel';
+title.innerHTML = 'this is a discription of the wonderful book called emmanuel is was published 2024';
+title.style.backgroundColor = '  lightBlue';
+title.style.borderRadius = '30px';
+title.style.fontSize = '30px';
+ul.appendChild(title);
